6334dcd014f36e423477074c0657a1e3bd9378a1
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-16 23:15:55 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,26 @@
 # Make your shoe class here!
+require 'pry'
+
+class Shoe
+
+  attr_accessor (:color), (:size), (:material), (:condition)
+  attr_reader (:brand)
+
+  def initialize(init_brand)
+    @brand = init_brand
+
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
+
+
+
+
+
+
+
+end
